85fc1d83ee6b8b2c7ddb62f10e6c2c80958b72f6
praveengamini <132166319+praveengamini@users.noreply.github.com>
2023-10-26 20:53:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+$(".mobile-nav-toggle").click(function() {
+    $(".primary-navigation").css("visibility", "visible");
+});
+$(".mobile-nav-toggle").click(function() {
+    $(".primary-navigation").toggle();
+    
+});
+$(".mobile-nav-toggle").click(function() {
+    $(".mobile-nav-toggle").css("background-image", "url(closing.png)");
+});
+$(".mobile-nav-toggle").click(function() {
+    $(".mobile-nav-toggle").toggle;
+});
+
